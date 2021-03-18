c487b01e297bed11f018e5c60123a3abd6e8c40d
Tom Tuning <Thomas.E.Tuning@gmail.com>
2021-03-18 16:49:22 -0400

    run in background

--- KafkaScripts/run-cluster.sh
+++ KafkaScripts/run-cluster.sh
@@ -1,11 +1,18 @@
 #Copyright © 2019, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
 #SPDX-License-Identifier: Apache-2.0
 
-./run-zookeeper.sh
+echo "Starting Zookeeper"
+nohup ./run-zookeeper.sh > /tmp/$(date +%s)zookeeper.log 2>&1 &
 sleep 9
-./run-server0.sh
+echo "Starting Broker0"
+
+nohup ./run-server0.sh > /tmp/$(date +%s)server0.log 2>&1 &
 sleep 2
-./run-server1.sh
+echo "Starting Broker1"
+
+nohup ./run-server1.sh > /tmp/$(date +%s)server1.log 2>&1 &
 sleep 2
-./run-server2.sh
+echo "Starting Broker2"
+
+nohup ./run-server2.sh > /tmp/$(date +%s)server2.log 2>&1 &
 

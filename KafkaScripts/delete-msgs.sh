9c0092eb4d451113fca06540e3aa4f2d368b25ad
Tom Tuning <Thomas.E.Tuning@gmail.com>
2021-03-18 15:33:41 -0400

    Update delete-msgs.sh

--- KafkaScripts/delete-msgs.sh
+++ KafkaScripts/delete-msgs.sh
@@ -1,15 +1,15 @@
-#!/usr/bin/env bash 
+#!/usr/bin/bash 
 
-#Copyright © 2019, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
+#Copyright © 2021, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved....
 #SPDX-License-Identifier: Apache-2.0
+
+source ./env_vars
 #
 #   This changes the message retention period to 1 second.  
 #  After 1 second old messages will be deleted.  
 #  Make sure to reset the retention period after msg purge
 #
-cd /opt/kafka/
-BPATH=/opt/kafka/bin
-SPATH=/opt/kafka/myscripts
+
 echo $1
 
 if [[ "$#" != 1                  ]] ;  then 

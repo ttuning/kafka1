58409a902cdbc7820dd83191f886b6c416c0b967
Tom Tuning <Thomas.E.Tuning@gmail.com>
2021-03-18 14:50:31 -0400

    Update list-topics.sh

--- KafkaScripts/list-topics.sh
+++ KafkaScripts/list-topics.sh
@@ -1,25 +1,11 @@
-<<<<<<< HEAD
 #!/usr/bin/bash 
 
 #Copyright © 2019, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved....
 #SPDX-License-Identifier: Apache-2.0
+
 source /home/jumpuser/esp-high-availability-using-kafka-and-viya-4/KafkaScripts/env_vars
-cat  /home/jumpuser/esp-high-availability-using-kafka-and-viya-4/KafkaScripts/env_vars
 
 echo  "show the SPATH"
 echo $SPATH
 
-#$BPATH/kafka-topics.sh --list --zookeeper localhost:2181
-=======
-#!/usr/bin/bash 
-
-#Copyright © 2019, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved....
-#SPDX-License-Identifier: Apache-2.0
-#source /home/jumpuser/esp-high-availability-using-kafka-and-viya-4/KafkaScripts/env_vars
-cat  /home/jumpuser/esp-high-availability-using-kafka-and-viya-4/KafkaScripts/env_vars
-
-echo  "show the SPATH"
-echo $SPATH
-
-#$BPATH/kafka-topics.sh --list --zookeeper localhost:2181
->>>>>>> 8cc937eb36f54b2551476ac7882658ba71cb657a
+$BPATH/kafka-topics.sh --list --zookeeper localhost:2181

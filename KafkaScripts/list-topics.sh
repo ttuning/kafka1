02672e38ef0948afe5a2f84df347c7db1b3f7f5f
Tom Tuning <Thomas.E.Tuning@gmail.com>
2021-03-18 13:51:25 -0400

    Update list-topics.sh

--- KafkaScripts/list-topics.sh
+++ KafkaScripts/list-topics.sh
@@ -3,7 +3,9 @@
 #Copyright © 2019, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved....
 #SPDX-License-Identifier: Apache-2.0
 source /home/jumpuser/esp-high-availability-using-kafka-and-viya-4/KafkaScripts/env_vars
+cat  /home/jumpuser/esp-high-availability-using-kafka-and-viya-4/KafkaScripts/env_vars
 
+echo  "show the SPATH"
 echo $SPATH
 
 $BPATH/kafka-topics.sh --list --zookeeper localhost:2181

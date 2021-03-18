112e9574cfc56c21279baad6b326fca083674562
Tom Tuning <Thomas.E.Tuning@gmail.com>
2021-03-18 17:18:47 -0400

    {LOGTIME}

--- KafkaScripts/run-cluster.sh
+++ KafkaScripts/run-cluster.sh
@@ -4,24 +4,24 @@
 #SPDX-License-Identifier: Apache-2.0
 
 LOGTIME=$(date +%s)
-echo log location /tmp/$(LOGTIME)xxxxxxx.log
+echo log location /tmp/${LOGTIME}xxxxxxx.log
 
 echo Starting Zookeeper \a
-nohup ./run-zookeeper.sh > /tmp/$(LOGTIME)zookeeper.log 2>&1 &
+nohup ./run-zookeeper.sh > /tmp/${LOGTIME}zookeeper.log 2>&1 &
 sleep 19
 
 echo "Starting Broker0" \a
 
-nohup ./run-server0.sh > /tmp/$(LOGTIME)server0.log 2>&1 &
+nohup ./run-server0.sh > /tmp/${LOGTIME}server0.log 2>&1 &
 sleep 2
 echo "Starting Broker1" \a
 
-nohup ./run-server1.sh > /tmp/$(LOGTIME)server1.log 2>&1 &
+nohup ./run-server1.sh > /tmp/${LOGTIME}server1.log 2>&1 &
 sleep 2
 
 echo "Starting Broker2" \a
 
-nohup ./run-server2.sh > /tmp/$(LOGTIME)server2.log 2>&1 &
+nohup ./run-server2.sh > /tmp/${LOGTIME}server2.log 2>&1 &
 
 ps -ef | grep run-
 

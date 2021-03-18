2846235ea66a1dd937798a7bff1dd47a633f81c2
Tom Tuning <Thomas.E.Tuning@gmail.com>
2021-03-18 15:25:29 -0400

    Update start-consumerIN.sh

--- KafkaScripts/start-consumerIN.sh
+++ KafkaScripts/start-consumerIN.sh
@@ -1,11 +1,10 @@
-#!/usr/bin/env bash 
+#!/usr/bin/bash 
 
-#Copyright © 2019, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
+#Copyright © 2021, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved....
 #SPDX-License-Identifier: Apache-2.0
 
-cd /opt/kafka/
-BPATH=/opt/kafka/bin
-SPATH=/opt/kafka/myscripts
+source ./env_vars
+
 
 $BPATH/kafka-console-consumer.sh \
 --bootstrap-server iotdemo.na.sas.com:9092,iotdemo.na.sas.com:9093,iotdemo.na.sas.com:9094 \

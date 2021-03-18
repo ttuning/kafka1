bdd19343bc974e38328b536f0014f749c992ae6e
Tom Tuning <Thomas.E.Tuning@gmail.com>
2021-03-18 15:28:16 -0400

    brokers

--- KafkaScripts/start-consumerOUT.sh
+++ KafkaScripts/start-consumerOUT.sh
@@ -1,14 +1,13 @@
-#!/usr/bin/env bash 
+#!/usr/bin/bash 
 
-#Copyright © 2019, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
+#Copyright © 2021, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved....
 #SPDX-License-Identifier: Apache-2.0
 
-cd /opt/kafka/
-BPATH=/opt/kafka/bin
-SPATH=/opt/kafka/myscripts
+source ./env_vars
+
 
 $BPATH/kafka-console-consumer.sh \
---bootstrap-server iotdemo.na.sas.com:9092,iotdemo.na.sas.com:9093 \
+--bootstrap-server $KBROKER0,$KBROKER1,$KBROKER2 \
 --topic iotdemo.na.sas.com_61001.kafka1.cq1.kafka_outgoing.O \
 --from-beginning
 

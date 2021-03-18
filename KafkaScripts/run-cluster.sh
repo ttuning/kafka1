7e2840c86b8a98593fddf00d3b85a62618aec0e3
Tom Tuning <Thomas.E.Tuning@gmail.com>
2021-03-18 17:14:02 -0400

    times

--- KafkaScripts/run-cluster.sh
+++ KafkaScripts/run-cluster.sh
@@ -1,18 +1,27 @@
-#Copyright © 2019, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
+#!/usr/bin/bash
+
+#Copyright © 2021, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
 #SPDX-License-Identifier: Apache-2.0
 
-echo "Starting Zookeeper"
-nohup ./run-zookeeper.sh > /tmp/$(date +%s)zookeeper.log 2>&1 &
-sleep 9
-echo "Starting Broker0"
+LOGTIME=$(date +%s)
+echo log location /tmp/$(LOGTIME)xxxxxxx.log
 
-nohup ./run-server0.sh > /tmp/$(date +%s)server0.log 2>&1 &
+echo Starting Zookeeper \a
+nohup ./run-zookeeper.sh > /tmp/$(LOGTIME)zookeeper.log 2>&1 &
+sleep 19
+
+echo "Starting Broker0" \a
+
+nohup ./run-server0.sh > /tmp/$(LOGTIME)server0.log 2>&1 &
 sleep 2
-echo "Starting Broker1"
+echo "Starting Broker1" \a
 
-nohup ./run-server1.sh > /tmp/$(date +%s)server1.log 2>&1 &
+nohup ./run-server1.sh > /tmp/$(LOGTIME)server1.log 2>&1 &
 sleep 2
-echo "Starting Broker2"
 
-nohup ./run-server2.sh > /tmp/$(date +%s)server2.log 2>&1 &
+echo "Starting Broker2" \a
+
+nohup ./run-server2.sh > /tmp/$(LOGTIME)server2.log 2>&1 &
+
+ps -ef | grep run-
 

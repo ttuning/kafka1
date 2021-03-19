fb88d9674185297dfd70d5ce67a9b4d04ff11177
Tom Tuning <Thomas.E.Tuning@gmail.com>
2021-03-19 14:32:26 -0400

    Update create-data.sh

--- ESPScripts/create-data.sh
+++ ESPScripts/create-data.sh
@@ -38,7 +38,7 @@ until [ $counter -gt $parg2 ]
 do 
 
     usedmemory=$(vmstat -s --unit M | grep -i "used memory" | cut -f 10 -d " ")
-    freememory=$(vmstat -s --unit M | grep -i "free memory" | cut  -d " " -f1 )	
+    freememory=$(vmstat -s --unit M | grep -i "free memory" | cut  -d " " -f 1 )	
     
     echo $prefix$usedmemory$del$freememory >> $outfile
     sleep $parg1

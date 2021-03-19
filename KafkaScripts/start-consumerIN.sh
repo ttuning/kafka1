148cffcacdc2a2fd2fc01c90445576b73c1664a6
Tom Tuning <Thomas.E.Tuning@gmail.com>
2021-03-19 11:25:42 -0400

    HAtopic1_31415.ESP_Project_HA_Kafka.ESP_CQ.ESP_incoming_source_window.I

--- KafkaScripts/start-consumerIN.sh
+++ KafkaScripts/start-consumerIN.sh
@@ -8,7 +8,7 @@ source ./env_vars
 
 $BPATH/kafka-console-consumer.sh \
 --bootstrap-server $KBROKER0,$KBROKER1,$KBROKER2 \
---topic ESP_Project_HA_Kafka.ESP_CQ.ESP_incoming_source_window.I \
+--topic HAtopic1_31415.ESP_Project_HA_Kafka.ESP_CQ.ESP_incoming_source_window.I \
 --from-beginning
 
  
